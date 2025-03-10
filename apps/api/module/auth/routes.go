7fbef5e124202b11914eee3340fa9bf7b8c4b40a
Lil-Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2025-03-10 14:05:54 -0600

    fix: url trailing forward slash

--- apps/api/module/auth/routes.go
+++ apps/api/module/auth/routes.go
@@ -3,10 +3,6 @@ package auth
 import "net/http"
 
 func SetupRoutes(parentMux *http.ServeMux) {
-	authMux := http.NewServeMux()
-
-	authMux.HandleFunc("GET /google", GetGoogleAuth)
-	authMux.HandleFunc("GET /google/callback", GetGoogleAuthCallback)
-
-	parentMux.Handle("/auth/", http.StripPrefix("/auth", authMux))
+	parentMux.HandleFunc("GET /auth/google", GetGoogleAuth)
+	parentMux.HandleFunc("GET /auth/google/callback", GetGoogleAuthCallback)
 }
--- apps/api/module/cat/routes.go
+++ apps/api/module/cat/routes.go
@@ -3,10 +3,6 @@ package cat
 import "net/http"
 
 func SetupRoutes(parentMux *http.ServeMux) {
-	catMux := http.NewServeMux()
-
-	catMux.HandleFunc("GET /", GetCats)
-	catMux.HandleFunc("POST /", PostCat)
-
-	parentMux.Handle("/cat/", http.StripPrefix("/cat", catMux))
+	parentMux.HandleFunc("GET /cat", GetCats)
+	parentMux.HandleFunc("POST /cat", PostCat)
 }
