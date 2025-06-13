f7f58431fd92a853a7315000a99ed9d7b956d3b6
Lil-Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2025-06-13 11:59:40 -0600

    feat: project page

--- apps/webapp/src/pages/project.tsx
+++ apps/webapp/src/pages/project.tsx
@@ -1,6 +1,7 @@
 import { GET, Project } from "@glassact/data";
 import { Breadcrumb, Button, cn } from "@glassact/ui";
 import { createSignal, Component, Index } from "solid-js";
+import { IoCheckmarkCircleOutline } from "solid-icons/io";
 
 const ProjectPage: Component = () => {
   const [selectedInlay, setSelectedInlay] = createSignal(0);
@@ -20,6 +21,18 @@ const ProjectPage: Component = () => {
 
   const inlays = ["1234-78-A21", "BIR-203-152"];
 
+  const steps = [
+    { name: "Proof Creation", href: "#", status: "complete" },
+    { name: "Proof Approval", href: "#", status: "current" },
+    { name: "Order Placement", href: "#", status: "upcoming" },
+    { name: "Material Prep", href: "#", status: "upcoming" },
+    { name: "Cutting", href: "#", status: "upcoming" },
+    { name: "Fire Polishing", href: "#", status: "upcoming" },
+    { name: "Packaging", href: "#", status: "upcoming" },
+    { name: "Shipped", href: "#", status: "upcoming" },
+    { name: "Delivered", href: "#", status: "upcoming" },
+  ];
+
   return (
     <div>
       <Breadcrumb
@@ -28,7 +41,6 @@ const ProjectPage: Component = () => {
           { title: project.name, href: `/projects/${project.uuid}` },
         ]}
       />
-
       <div class="relative border-b border-gray-200 pb-5 sm:pb-0">
         <div class="md:flex md:items-center md:justify-between">
           <h1 class="text-2xl font-bold text-gray-900">{project.name}</h1>
@@ -45,7 +57,7 @@ const ProjectPage: Component = () => {
                   <div
                     onClick={() => setSelectedInlay(index)}
                     class={cn(
-                      "cursor-pointer whitespace-nowrap border-b-2 border-primary px-1 pb-4 text-sm font-medium text-primary",
+                      "cursor-pointer whitespace-nowrap border-b-2 border-primary px-1 pb-2 text-sm font-medium text-primary",
                       index === selectedInlay()
                         ? "border-primary text-primary"
                         : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
@@ -58,6 +70,64 @@ const ProjectPage: Component = () => {
             </nav>
           </div>
         </div>
+      </div>
+      <div class="mt-8 flex gap-4">
+        <div>
+          <nav class="flex">
+            <ol role="list" class="space-y-6">
+              <Index each={steps}>
+                {(step) => (
+                  <li>
+                    {step().status === "complete" ? (
+                      <a href={step().href} class="group">
+                        <span class="flex items-start">
+                          <span class="relative flex size-5 shrink-0 items-center justify-center">
+                            <IoCheckmarkCircleOutline class="size-full text-primary group-hover:text-primary" />
+                          </span>
+                          <span class="ml-3 text-sm font-medium text-gray-500 group-hover:text-gray-900">
+                            {step().name}
+                          </span>
+                        </span>
+                      </a>
+                    ) : step().status === "current" ? (
+                      <a
+                        href={step().href}
+                        aria-current="step"
+                        class="flex items-start"
+                      >
+                        <span
+                          aria-hidden="true"
+                          class="relative flex size-5 shrink-0 items-center justify-center"
+                        >
+                          <span class="absolute size-4 rounded-full bg-red-100" />
+                          <span class="relative block size-2 rounded-full bg-primary" />
+                        </span>
+                        <span class="ml-3 text-sm font-medium text-primary">
+                          {step().name}
+                        </span>
+                      </a>
+                    ) : (
+                      <a href={step().href} class="group">
+                        <div class="flex items-start">
+                          <div
+                            aria-hidden="true"
+                            class="relative flex size-5 shrink-0 items-center justify-center"
+                          >
+                            <div class="size-2 rounded-full bg-gray-300 group-hover:bg-gray-400" />
+                          </div>
+                          <p class="ml-3 text-sm font-medium text-gray-500 group-hover:text-gray-900">
+                            {step().name}
+                          </p>
+                        </div>
+                      </a>
+                    )}
+                  </li>
+                )}
+              </Index>
+            </ol>
+          </nav>
+        </div>
+        <div class="border rounded-xl p-4 w-full">hello</div>
       </div>
     </div>
   );
