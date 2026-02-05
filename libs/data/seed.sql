bd39be5cffc5159f8d35046d1b42f26fa3ea243d
Lil-Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2026-02-05 11:47:34 -0700

    fix: update seed

--- libs/data/seed.sql
+++ libs/data/seed.sql
@@ -9,34 +9,27 @@ VALUES (
     'US',
     ST_SetSRID(ST_MakePoint(-111.72878560766672, 40.28727777344243), 4326)::GEOGRAPHY);
 
-INSERT INTO users (name, email, avatar, dealership_id, role)
+INSERT INTO internal_users (name, email, avatar, role)
 VALUES (
     'Aaron Santo',
     'santoaaron@gmail.com',
     'https://ui-avatars.com/api/?name=Aaron+Santo&background=BAFFC9',
-    1,
     'admin'
 );
 
-INSERT INTO users (name, email, avatar, dealership_id, role)
-VALUES (
-    'Aaron Santo',
-    'apenguinemail@gmail.com',
-    'https://ui-avatars.com/api/?name=Aaron+Santo&background=BAFFC9',
-    1,
-    'user'
-);
-
-INSERT INTO users (name, email, avatar, dealership_id, role)
+INSERT INTO internal_users (name, email, avatar, role)
 VALUES (
     'T8 Storey',
     't8storey@protonmail.com',
-    'https://ui-avatars.com/api/?name=Aaron+Santo&background=BAFFC9',
-    1,
+    'https://ui-avatars.com/api/?name=T8+Storey&background=BAFFC9',
     'admin'
 );
 
-INSERT INTO catalog_items (created_at)
+INSERT INTO dealership_users (dealership_id, name, email, avatar, role)
 VALUES (
-    now()
+    1,
+    'Aaron Santo',
+    'apenguinemail@gmail.com',
+    'https://ui-avatars.com/api/?name=Aaron+Santo&background=BAFFC9',
+    'admin'
 );
