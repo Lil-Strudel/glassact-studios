f586b4965fecc8a6963940fd67eccdfa2c8fa655
Lil-Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2026-04-23 18:30:20 -0600

    feat: 404 page on landing site

--- apps/infrastructure/landing.tf
+++ apps/infrastructure/landing.tf
@@ -88,6 +88,20 @@ resource "aws_cloudfront_distribution" "landing" {
     }
   }
 
+  custom_error_response {
+    error_code            = 403
+    response_code         = 404
+    response_page_path    = "/404.html"
+    error_caching_min_ttl = 10
+  }
+
+  custom_error_response {
+    error_code            = 404
+    response_code         = 404
+    response_page_path    = "/404.html"
+    error_caching_min_ttl = 10
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
